7f751ef765a3ff4ca6f5fcee1711523c349ab2dd
Sina Rastegar <github@sinarastegar.com>
2020-04-08 10:33:03 -0400

    fixed the x,y,x offsets to the current setup with a fixed bed

--- Configuration.h
+++ Configuration.h
@@ -953,7 +953,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 44, -2, -2.4 }
+#define NOZZLE_TO_PROBE_OFFSET { 44, -2, -1.9 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1095,8 +1095,8 @@
 #define Y_BED_SIZE 219
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -40
-#define Y_MIN_POS -6
+#define X_MIN_POS -43
+#define Y_MIN_POS -7
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
